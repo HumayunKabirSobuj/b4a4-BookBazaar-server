957150a45ff9d5ce74b47e0bea78a1c0e088e009
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-01-25 15:30:02 +0600

    Initial setup done

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+const app = express();
+// const port = 3000;
+app.use(cors());
+
+//parser
+app.use(express.json());
+
+//application Route
+// app.use('/api/v1/students', StudentRoutes);
+
+const getController = (req: Request, res: Response) => {
+  res.send('Hello World!');
+};
+app.get('/', getController);
+
+export default app;
+
