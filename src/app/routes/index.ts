2805521e05fffb34413a4ad476dbe3ec585d91ad
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-02-11 21:02:36 +0600

    Blog Data crud functionality added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,19 +1,13 @@
 import { Router } from 'express';
-import { AuthRoutes } from '../modules/auth/auth.route';
-import { productRoutes } from '../modules/productManagement/product.route';
+import { productRoutes } from '../modules/blogManagement/blog.route';
 
 const router = Router();
 
 const moduleRoutes = [
   {
-    path: '/auth',
-    route: AuthRoutes,
-  },
-  {
     path: '/product',
     route: productRoutes,
   },
- 
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
